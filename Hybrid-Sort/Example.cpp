e312228b93142a42ad5698b085d8983915f4a4b9
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-04-16 12:01:30 +0300

    Count sort deleted, added sorted elements percentage

---
+++ Hybrid-Sort/Example.cpp
@@ -0,0 +1,22 @@
+﻿//#include <iostream>
+//#include <vector>
+//#include <list>
+//#include "SortingAlgorithms.h"
+//
+//int main()
+//{
+//    std::vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//    HybridSort(arr, arr.size() - 1);
+//
+//    for (auto it = arr.begin(); it != arr.end(); ++it) {
+//        std::cout << *it << " ";
+//    }
+//
+//    std::vector<std::string> list = { "something", "text", "Test", "DSA", "cat", "dog" };
+//    HybridSort(list, list.size() - 1);
+//
+//    std::cout << std::endl;
+//    for (auto it = list.begin(); it != list.end(); ++it) {
+//        std::cout << *it << " ";
+//    }
+//}
